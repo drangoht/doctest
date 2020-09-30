bc21a62a924feb9cac21895d1b06a80bb9bc7fd4
drangoht <drangoht@gmail.com>
2020-09-30 10:27:34 +0200

    solution pour doc test

---
+++ doctest/doctest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace doctest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var res = Math.Divide(1, 2);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
